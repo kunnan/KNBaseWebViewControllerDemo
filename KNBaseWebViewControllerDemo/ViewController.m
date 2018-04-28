76346d990c3f60d0a765a3b7df6b47a58e449d1e
zhangkn <zhangkunnan@qinbaowan.com>
2018-04-28 13:04:28 +0800

    Podfile

--- KNBaseWebViewControllerDemo/ViewController.m
+++ KNBaseWebViewControllerDemo/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import <KNBaseWebViewController/KNBaseWebViewController-umbrella.h>
 @interface ViewController ()
 
 @end
@@ -19,10 +19,20 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    NSLog(@"touchesBegan");
+    //    [self setupFeedback];
+    [self setupKNTestWebViewController];
+}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+//app嵌入H5页面的通用模版的 例子
+- (void)setupKNTestWebViewController{
+    KNBaseWebViewController *vc = [[KNBaseWebViewController alloc]init];
+    
+    [self presentViewController:vc animated:YES completion:^{
+        
+    }];
 }
 
 
--- Podfile
+++ Podfile
@@ -6,5 +6,6 @@ source 'https://github.com/zhangkn/Specs.git'
 target 'KNBaseWebViewControllerDemo' do
   # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
   use_frameworks!
-   pod 'KNBaseWebViewController'
+  pod 'KNBaseWebViewController' # pod update
+#   pod 'KNBaseWebViewController' , '~> 0.1.2'
 end
